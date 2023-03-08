9d09cb0e82f6aa70b01479ab1d661f8870e525ac
FredyRosero <FredyRosero@users.noreply.github.com>
2023-03-08 15:19:38 +0000

    deploy: c3c452e5ad25db3910b14cfa6bd9e9327aa2f9e4

--- sketches/helloworld.js
+++ sketches/helloworld.js
@@ -1,17 +1,8 @@
-let inconsolata;
-function preload() {
-  inconsolata = loadFont('assets/inconsolata.otf');
-}
-function setup() {
-  createCanvas(100, 100, WEBGL);
-  textFont(inconsolata);
-  textSize(width / 3);
-  textAlign(CENTER, CENTER);
-}
-function draw() {
-  background(0);
-  let time = millis();
-  rotateX(time / 1000);
-  rotateZ(time / 1234);
-  text('p5.js', 0, 0);
-}
+const s = ( sketch ) => {
+  let x = 100;
+  let y = 100;
+
+  sketch.draw = function() {   // draw() is an inner function, a "closure"
+    sketch.rect(x,y,50,50);    // draw() uses variables (x,y) declared in the parent function s
+  };
+};
