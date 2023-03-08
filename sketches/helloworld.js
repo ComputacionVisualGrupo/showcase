9d7a8f55443743d751ad6c145474ed82592c9c5f
FredyRosero <FredyRosero@users.noreply.github.com>
2023-03-08 15:24:49 +0000

    deploy: 2d0c2868a436c3cfef931c8d61e530dff869d5ed

--- sketches/helloworld.js
+++ sketches/helloworld.js
@@ -1,8 +1,35 @@
-const s = ( sketch ) => {
-  let x = 100;
-  let y = 100;
-
-  sketch.draw = function() {   // draw() is an inner function, a "closure"
-    sketch.rect(x,y,50,50);    // draw() uses variables (x,y) declared in the parent function s
+new p5((p) => {
+  p.setup = function () {
+    p.createCanvas(400, 400);
+    p.strokeWeight(3); // medium weight lines
+    p.smooth(); // antialias lines
+    p.noLoop();
   };
-};
+
+  p.draw = function () {
+    p.background(0);
+    p.stroke(100, 100, 100); // dark grey colour for lines
+
+    var step = 25;
+    //vertical lines
+    for (var x = step; x < p.width; x = x + step) {
+      p.line(x, 0, x, p.height);
+    }
+
+    //horizontal lines
+    for (var y = step; y < p.height; y = y + step) {
+      p.line(0, y, p.width, y);
+    }
+
+    // Circles
+    p.ellipseMode(p.CENTER);
+    p.stroke(255, 255, 255); // white circles
+    for (var i = step; i < p.width - 5; i = i + step) {
+      for (var j = step; j < p.height - 15; j = j + step) {
+        p.strokeWeight(6);
+        p.point(i, j);
+        p.strokeWeight(3);
+      }
+    }
+  };
+}, "holamundo");
