939c37866794b165f34a64bd40d68a7a07963b95
FredyRosero <FredyRosero@users.noreply.github.com>
2023-03-15 15:51:40 +0000

    deploy: 8afe5882fbe95331a51f1a4d37218aa7033c1fc1

--- sketches/loadimage.js
+++ sketches/loadimage.js
@@ -7,9 +7,6 @@ function setup() {
 }
 
 function draw() {
-  // Muestra la imagen en su tamaño original en la posición (0,0)
   image(img, 0, 0);
-  // Muestra la imagen en la posición (0, height/2) a la mitad del tamaño
-  image(img, 0, height / 2, img.width / 2, img.height / 2);
 }
 
